76a532f7b06404604f6492be4bed03bf692cae9a
minleejae <mmj9808@naver.com>
2023-03-01 15:27:40 +0900

    FEATURE: TypeOrm imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [ConfigModule.forRoot({
-        isGlobal: true
-    })],
+    imports: [
+        /* .env 파일 사용하기 위한 모듈 전역으로 설정 */
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+
+        /* TypeOrm */
+        TypeOrmModule.forRoot({
+            type: process.env.TYPEORM_TYPE as any,
+            host: process.env.TYPEORM_HOST,
+            port: Number(process.env.TYPEORM_PORT),
+            username: process.env.TYPEORM_USERNAME,
+            password: process.env.TYPEORM_PASSWORD,
+            database: process.env.TYPEORM_DATABASE,
+            entities: [],
+            synchronize: true, //배포시 false로 바꿔야함
+        }),
+
+
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
