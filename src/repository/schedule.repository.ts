dbc83b22e95027c768f6970a682f88cf4915f9ec
minleejae <mmj9808@naver.com>
2023-03-13 21:41:59 +0900

    feature : 스케줄 create api 추가

--- src/repository/schedule.repository.ts
+++ src/repository/schedule.repository.ts
@@ -1,31 +1,49 @@
 import { HttpException, HttpStatus } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { CategoriesService } from "src/categories/categories.service";
 import { Alarm } from "src/entity/alarm.entity";
+import { Category } from "src/entity/category.entity";
 import { Schedule } from "src/entity/schedule.entity";
 import { CreateScheduleDto } from "src/schedules/dto/create.schedule.dto";
 import { Repository } from "typeorm";
 
 export class ScheduleRepository {
     constructor(@InjectRepository(Schedule) private readonly repository: Repository<Schedule>,
+            private readonly categoriesService:CategoriesService
     ) { }
 
+    /* 스케줄 데이터 저장 */
     async createSchedule(userId: string, createScheduleDto: CreateScheduleDto){
         const queryRunner = this.repository.manager.connection.createQueryRunner();
         await queryRunner.connect();
         await queryRunner.startTransaction();
 
         try {
-            /* 스케줄 데이터 저장 */
-            console.log(createScheduleDto)
-
+            /* 카테고리 저장 */
+            const [category] = await this.categoriesService.createCategories(userId, { contents : [createScheduleDto.category]})
+        
+            /* 스케줄 저장 */
             const savedSchedule = await queryRunner.manager.save(Schedule, {
                 ...createScheduleDto,
                 user: userId,
+                category : {
+                    id : category.id,
+                    content : category.content
+                }
             });
+
+            /* 스케줄 알람 저장 */
+            const newAlarms = createScheduleDto.alarms.map((alarm) => ({
+                user: userId,
+                schedule: savedSchedule.id,
+                time: alarm,
+            }));
+            const savedAlarms = await queryRunner.manager.save(Alarm, newAlarms);
+            const retAlarms = savedAlarms.map(({ id, time }) => ({ id, time }));
 
             await queryRunner.commitTransaction();
 
-            // return { ...savedSchedule, alarms: retAlarms };
+            return {...savedSchedule, alarms: retAlarms};
         } catch (error) {
             await queryRunner.rollbackTransaction();
             throw new HttpException(
--- src/schedules/schedules.module.ts
+++ src/schedules/schedules.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesService } from 'src/categories/categories.service';
 import { Alarm } from 'src/entity/alarm.entity';
 import { Category } from 'src/entity/category.entity';
 import { Schedule } from 'src/entity/schedule.entity';
 import { SubTodo } from 'src/entity/sub-todo.entity';
 import { User } from 'src/entity/user.entity';
+import { CategoryRepository } from 'src/repository/category.repository';
 import { ScheduleRepository } from 'src/repository/schedule.repository';
 import { ScheduleController } from './schedules.controller';
 import { ScheduleService } from './schedules.service';
@@ -12,7 +14,7 @@ import { ScheduleService } from './schedules.service';
 @Module({
     imports: [TypeOrmModule.forFeature([User, SubTodo, Category, Alarm, Schedule])],
     controllers: [ScheduleController],
-    providers: [ScheduleService, ScheduleRepository]
+    providers: [ScheduleService, ScheduleRepository, CategoriesService, CategoryRepository]
   
 })
 export class SchedulesModule {}
