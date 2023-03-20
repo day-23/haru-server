c90ecd693b7b56a2dbe10058c6f4f2087cf68868
minleejae <mmj9808@naver.com>
2023-03-20 16:44:02 +0900

    feature : ci cd test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Haru!';
+    return 'Hello Haru! with CI CD';
   }
 }
