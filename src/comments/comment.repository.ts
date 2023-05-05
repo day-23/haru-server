ebb546ed2de1d0f0a8855f4d1b1eb3999044d982
minleejae <mmj9808@naver.com>
2023-05-05 15:46:42 +0900

    fix : 전체 댓글 리스트에 프로필이미지 추가

--- src/comments/comment.repository.ts
+++ src/comments/comment.repository.ts
@@ -1,13 +1,17 @@
 import { HttpException, HttpStatus } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { InjectRepository } from "@nestjs/typeorm";
 import { CreateCommentDto, CreateImageCommentDto, UpdateCommentDto } from "src/comments/dto/create.comment.dto";
 import { ImageCommentCreateResponse, GetCommentsPaginationResponse, CommentCreateResponse } from "src/comments/interface/comment.interface";
 import { PaginationDto } from "src/common/dto/pagination.dto";
+import { getS3ImageUrl } from "src/common/utils/s3";
 import { Comment } from "src/entity/comment.entity";
 import { Repository } from "typeorm";
 
 export class CommentRepository {
-    constructor(@InjectRepository(Comment) private readonly repository: Repository<Comment>) { }
+    constructor(@InjectRepository(Comment) private readonly repository: Repository<Comment>,
+        private readonly configService: ConfigService) {
+    }
 
     async createComment(userId: string, postId: string, createCommentDto: CreateCommentDto): Promise<CommentCreateResponse> {    
         const { content } = createCommentDto
@@ -45,14 +49,51 @@ export class CommentRepository {
         const { page, limit } = paginationDto;
         const skip = (page - 1) * limit;
 
-        const [comments, count] = await this.repository.createQueryBuilder('comment')
-            .leftJoinAndSelect('comment.user', 'user')
-            .skip(skip)
-            .take(limit)
-            .select(['comment.id', 'comment.content', 'comment.x', 'comment.y', 'comment.createdAt', 'comment.updatedAt'])
-            .addSelect(['user.id', 'user.name'])
-            .orderBy('comment.createdAt', 'DESC')
-            .getManyAndCount();
+        const rawResult = await this.repository.manager.query(`
+            SELECT 
+            comment.id, 
+            comment.content, 
+            comment.x, 
+            comment.y, 
+            comment.created_at createdAt, 
+            comment.updated_at updatedAt, 
+            user.id AS userId, 
+            user.name, 
+            (
+                SELECT profileImage.url
+                FROM image profileImage
+                WHERE profileImage.user_id = user.id
+                ORDER BY profileImage.created_at DESC
+                LIMIT 1
+            ) AS profileImage
+            FROM 
+                comment
+                LEFT JOIN user ON comment.user_id = user.id
+            ORDER BY 
+                comment.created_at DESC
+            LIMIT ?, ?;
+        `, [skip, limit]);
+
+        const comments = rawResult.map(row => {
+            return {
+                id: row.id,
+                user: {
+                    id: row.userId,
+                    name: row.name,
+                    profileImage: getS3ImageUrl(this.configService, row.profileImage),
+                },
+                content: row.content,
+                x: row.x,
+                y: row.y,
+                createdAt: row.createdAt,
+                updatedAt: row.updatedAt,
+            };
+        });
+
+        const rawCount = await this.repository.manager.query(`
+            SELECT COUNT(*) as count FROM comment
+        `);
+        const count = Number(rawCount[0].count)
 
         const totalPages = Math.ceil(count / limit);
         return {
@@ -60,8 +101,8 @@ export class CommentRepository {
                 id: comment.id,
                 user: comment.user,
                 content: comment.content,
-                x : comment.x,
-                y : comment.y,
+                x: comment.x,
+                y: comment.y,
                 createdAt: comment.createdAt,
                 updatedAt: comment.updatedAt
             })),
