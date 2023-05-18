3620f50dd13b18a970d054eb69ae17bf437f0ef6
minleejae <mmj9808@naver.com>
2023-05-19 01:37:20 +0900

    fix : pagination skip error

--- src/posts/post.util.ts
+++ src/posts/post.util.ts
@@ -1,5 +1,6 @@
 export function calculateSkip(page: number, limit: number) {
-    let skip = (page - 1) * limit;
+    let skip = (page - 1) * limit - 1;
+    
     skip = Math.max(0, skip);
     return skip;
 }
