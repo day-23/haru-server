3c0e903ea16299986e551f65784c02ad20f71b7c
minleejae <mmj9808@naver.com>
2023-06-14 03:45:28 +0900

    feature : 애플 회원 탈퇴 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,7 @@
 import { Body, Controller, Get, Headers, Post, Req, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { User } from 'src/entity/user.entity';
 import { UserService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
-import { GoogleAuthGuard } from './guards/google-auth.guard';
-import { KakaoAuthGuard } from './guards/kakao-auth.guard';
-import { NaverAuthGuard } from './guards/naver-auth.guard';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { PostService } from 'src/posts/posts.service';
 
 @ApiTags('유저 인증 API')
@@ -61,67 +56,5 @@ export class AuthController {
        return await this.authService.validateAppleUser(authCode);
     }
 
-    // @Get('kakao/callback')
-    // @UseGuards(KakaoAuthGuard)
-    // async kakaoCallback(@Req() req, @Res() res) {
-    //     let user: User = await this.userService.getUserByEmail(req.user.email);
 
-    //     if (!user) {
-    //         user = await this.authService.signUp(req.user.email, req.user.name);
-    //     }
-
-    //     const { cookie, accessToken } = await this.authService.getAccessToken(
-    //         user,
-    //     );
-
-    //     res.setHeader('Set-Cookie', cookie);
-    //     return res.send(accessToken);
-    // }
-
-
-    // @Get('google')
-    // @UseGuards(GoogleAuthGuard)
-    // async googleLogin(@Req() req) {}
-
-    // @Get('google/callback')
-    // @UseGuards(GoogleAuthGuard)
-    // async googleCallback(@Req() req, @Res() res) {
-    //     let user: User = await this.userService.getUserByEmail(req.user.email);
-
-    //     if (!user) {
-    //         user = await this.authService.signUp(req.user.email, req.user.name);
-    //     }
-
-    //     const { cookie, accessToken } = await this.authService.getAccessToken(
-    //         user,
-    //     );
-
-    //     res.setHeader('Set-Cookie', cookie);
-    //     console.log('google Login');
-    //     return res.send(accessToken);
-    // }
-
-    // @Get('naver')
-    // @UseGuards(NaverAuthGuard)
-    // async naverLogin(@Req() req) {}
-
-    // @Get('naver/callback')
-    // @UseGuards(NaverAuthGuard)
-    // async naverCallback(@Req() req, @Res() res) {
-    //     let user: User = await this.userService.getUserByEmail(req.user.email);
-
-    //     if (!user) {
-    //         user = await this.authService.naverGetUserInfo(
-    //             req.user.accessToken,
-    //         );
-    //     }
-
-    //     const { cookie, accessToken } = await this.authService.getAccessToken(
-    //         user,
-    //     );
-
-    //     res.setHeader('Set-Cookie', cookie);
-    //     console.log('naver Login');
-    //     return res.send(accessToken);
-    // }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { User } from 'src/entity/user.entity';
 import { UserRepository } from 'src/users/user.repository';
 import { CreateUserDto, UpdateUserDto, UpdateUserOptionPartialDto } from './dto/users.dto';
-
+import * as jwt from 'jsonwebtoken';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class UserService {
-    constructor(private readonly userRepository: UserRepository
+    constructor(private readonly userRepository: UserRepository,
+        private readonly httpService: HttpService,
     ) { }
 
     async findOne(id: string): Promise<User> {
@@ -30,6 +32,45 @@ export class UserService {
     }
 
     async deleteUser(id: string): Promise<void> {
+        return await this.userRepository.delete(id);
+    }
+
+    async deleteAppleUser(id: string, accessToken: string, refreshToken: string): Promise<void> {
+        //apple revoke token then delete id
+        const appleIdUrl = 'https://appleid.apple.com/auth/revoke';
+        const clientId = process.env.APPLE_CLIENT_ID;
+        const teamId = process.env.APPLE_TEAM_ID;
+        const keyId = process.env.APPLE_KEY_ID;
+
+        // The private key contents are stored as a Base64 encoded string
+        const keyFileContentBase64 = process.env.APPLE_PRIVATE_KEY_BASE64;
+        // Decode the Base64 string to get the actual key contents
+        const keyFileContent = keyFileContentBase64.replace(/\\n/g, '\n');
+        
+        // Generate the client secret
+        const clientSecret = jwt.sign({}, keyFileContent, {
+            algorithm: 'ES256',
+            expiresIn: '1h', // Apple accepts client secret JWTs with an expiry time of up to 6 months.
+            audience: 'https://appleid.apple.com',
+            issuer: teamId,
+            subject: clientId,
+            keyid: keyId
+        });
+
+        const tokens = [accessToken, refreshToken]
+
+        tokens.forEach(async (token) => {
+            const params = new URLSearchParams();
+            params.append('client_id', clientId);
+            params.append('client_secret', clientSecret);
+            params.append('token', token);
+            try {
+                const response = await this.httpService.post(appleIdUrl, params).toPromise();
+            } catch (error) {
+                console.error(error.message);
+                console.error(error.response?.data);  // Log the response body
+            }
+        })
         return await this.userRepository.delete(id);
     }
 
