ba1ecaaee3aa0679660d8096b0155a5c8438bd17
minleejae <mmj9808@naver.com>
2023-10-15 12:45:45 +0900

    deploy : server test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Hello Haru! deploy! ^_^';
+        return 'Hello Haru! deploy! ^_^ 20231015';
     }
 }
